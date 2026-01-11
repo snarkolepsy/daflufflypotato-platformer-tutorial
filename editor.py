d6665513e52c7304c91faa88f1c8bb5b4df4332a
David Dovo <david.pham.do@gmail.com>
2026-01-10 21:48:19 -0800

    More map editor updates and refinements
    More updates the the map editor program. Allowed placement of tiles that snap to the grid as well as sprites that can be freely placed off-grid. Implemented WASD camera panning controls. Confirmed correct tile placement and deletion behavior.

--- editor.py
+++ editor.py
@@ -24,41 +24,63 @@ class Editor:
         self.tilemap = Tilemap(self, tile_size=16)
 
         # Need a method for moving the camera around in x and y axes
-        self.movement = [False, False, False, False]
+        self.movement = [False, False, False, False] # left, right, up, down
         self.scroll = [0, 0]
 
         # Iterable list of tiles we can cycle through and place with mouseclick
         self.tile_list = list(self.assets)
         self.tile_group = 0 # Index for the list
         self.tile_variant = 0 # For tiles that have alternative versions
+
+        # Alternative clicks and modifying keys for variant tiles
         self.clicking = False
         self.right_clicking = False
         self.shift = False
+        self.ongrid = True
 
     def run(self):
         while True:
             # Black background
             self.display.fill((0, 0, 0))
 
+            # Moving the camera based on key input
+            self.scroll[0] += (self.movement[1] - self.movement[0]) * 2 # Right - left
+            self.scroll[1] += (self.movement[3] - self.movement[2]) * 2 # Down - Up
+
             # Rendering the tilemap
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
             self.tilemap.render(self.display, offset=render_scroll)
 
-            # Placing the tile wherever we left-click
+            # Currently selected tile
+            current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
+            current_tile_img.set_alpha(100)
+
+            # Getting the current mouse position and converting it into grid coordinates
             mpos = pygame.mouse.get_pos()
             mpos = (mpos[0] / RENDER_SCALE, mpos[1] / RENDER_SCALE)
             tile_pos = (int(mpos[0] + self.scroll[0]) // self.tilemap.tile_size, int(mpos[1] + self.scroll[1]) // self.tilemap.tile_size)
-            if self.clicking:
+
+            # Previewing the tile we're about to put down
+            if self.ongrid: # Snapped to the tile grid
+                self.display.blit(current_tile_img, (tile_pos[0] * self.tilemap.tile_size - self.scroll[0], tile_pos[1] * self.tilemap.tile_size - self.scroll[1]))
+            else: # Unrestrained by the tile grid
+                self.display.blit(current_tile_img, mpos)
+
+            # Placing the tile wherever we left-click
+            if self.clicking and self.ongrid:
                 self.tilemap.tilemap[str(tile_pos[0]) + ';' + str(tile_pos[1])] = {'type' : self.tile_list[self.tile_group], 'variant' : self.tile_variant, 'pos' : tile_pos} # {'type' : 'stone', 'variant' : 1, 'pos' : (10, i + 5)}
             # Deleting tiles, if any, wherever we right-click
             if self.right_clicking:
                 tile_loc = str(tile_pos[0]) + ';' + str(tile_pos[1])
-                if tile_loc in self.tilemap.tilemap:
+                if tile_loc in self.tilemap.tilemap: # Deleting tiles that are snapped to the grid
                     del self.tilemap.tilemap[tile_loc]
+                for tile in self.tilemap.offgrid_tiles.copy(): # Deleting off-grid tiles
+                    tile_img = self.assets[tile['type']][tile['variant']]
+                    tile_r = pygame.Rect(tile['pos'][0] - self.scroll[0], tile['pos'][1] - self.scroll[1], tile_img.get_width(), tile_img.get_height())
+                    if tile_r.collidepoint(mpos):
+                        self.tilemap.offgrid_tiles.remove(tile)
 
-            # Show the current tile selected
-            current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
-            current_tile_img.set_alpha(100)
+            # Show the current tile selected in the top left hand corner of the screen
             self.display.blit(current_tile_img, (5, 5))
 
             for event in pygame.event.get():
@@ -71,6 +93,9 @@ class Editor:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.clicking = True
+                        # Putting down offgrid tiles, one sprite per click instead of holding down and dragging
+                        if not self.ongrid:
+                            self.tilemap.offgrid_tiles.append({'type' : self.tile_list[self.tile_group], 'variant' : self.tile_variant, 'pos' : (mpos[0] + self.scroll[0], mpos[1] + self.scroll[1])})
                     if event.button == 3:
                         self.right_clicking = True
                     if self.shift: # If holding shift, using the variant tiles instead
@@ -93,24 +118,26 @@ class Editor:
 
                 # Moving around in the level editor with the arrow keys
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_a:
                         self.movement[0] = True
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_d:
                         self.movement[1] = True
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_w:
                         self.movement[2] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_s:
                         self.movement[3] = True
+                    if event.key == pygame.K_g:
+                        self.ongrid = not self.ongrid
                     if event.key == pygame.K_LSHIFT:
                         self.shift = True
-                if event.type == pygame.KEYUP:
-                    if event.key ==  pygame.K_LEFT:
+                if event.type == pygame.KEYUP: # Releasing arrow keys stops camera motion
+                    if event.key ==  pygame.K_a:
                         self.movement[0] = False
-                    if event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_d:
                         self.movement[1] = False
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_w:
                         self.movement[2] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_s:
                         self.movement[3] = False
                     if event.key == pygame.K_LSHIFT:
                         self.shift = False
